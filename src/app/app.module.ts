dcd49bdff06aba95550862f7f9d933b1f13017e3
Alex Ramirez <alexraab08@gmail.com>
2023-06-11 18:25:31 -0500

    Fin de sección 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HeroComponent } from './heroes/hero/hero.component';
-import { ListaComponent } from './heroes/lista/lista.component';
+
 import { CounterModule } from './counter/counter.module';
+import { HeroesModule } from './heroes/heroes.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroComponent,
-    ListaComponent
   ],
   imports: [
     BrowserModule,
-    CounterModule
+    CounterModule,
+    HeroesModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
